af476fd41744dd24a5cd31c537388e35ffc276b4
Furkan Yıldız <93140144+FyPap@users.noreply.github.com>
2024-03-17 14:37:02 +0300

    notification system updated for socket io

--- socket.js
+++ socket.js
@@ -1,18 +1,30 @@
-// socket.js
 const socketIo = require("socket.io");
 let io;
 
-module.exports = {
-  init: (httpServer) => {
-    console.log("Socket.io initialized..");
+const initSocket = (httpServer) => {
+  console.log("\n\n _SOCKET1: Socket.io initialized..");
 
-    io = socketIo(httpServer);
-    return io;
-  },
-  getIO: () => {
-    if (!io) {
-      throw new Error("Socket.io not initialized!");
-    }
-    return io;
-  },
+  io = socketIo(httpServer);
+
+  return io;
+};
+
+const getIO = () => {
+  if (!io) {
+    throw new Error("\n\n _SOCKET2: Socket.io not initialized!");
+  }
+  return io;
+};
+
+const setIO = (newIo) => {
+  io = newIo;
+  this.io = newIo;
+  
+  return io;
+};
+
+module.exports = {
+  initSocket,
+  getIO,
+  setIO,
 };
