63ef7d13a59709d8ab6fb2ad2fac3767dba93f9e
digitsontr <admin@digitson.com.tr>
2024-03-22 23:32:28 +0000

    Update Artifacts

--- dist/routes/admin.js
+++ dist/routes/admin.js
@@ -74,11 +74,7 @@ router.get("/articles", adminTokenControl, upload.none(), async (req, res) => {
 });
 router.get("/logs", adminTokenControl, upload.none(), async (req, res) => {
   try {
-    //console.log("XXXXXXXXXXXXX1");
-
     const logs = await Log.findAll();
-
-    //console.log("0000000000");
     res.status(200).json(logs);
   } catch (error) {
     res.status(500).json({
